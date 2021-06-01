6a373a7b03a447c21948ad55f947ab04d6753256
Ernad Husremovic <hernad@bring.out.ba>
2021-06-01 14:00:08 +0200

    build .net 5.0.6 linux

---
+++ consoleapp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace consoleapp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".NET Version: {0}", Environment.Version.ToString());
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
